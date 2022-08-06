a2aca0a5d9b211be90f4560367b8b6e3c76dcf33
Damodar Lohani <dlohani48@gmail.com>
2022-08-06 12:28:26 +0000

    hex color validator

---
+++ lib/src/validators/hex_color.dart
@@ -0,0 +1,28 @@
+import 'types.dart';
+import 'validator.dart';
+
+class HexColor extends Validator {
+  @override
+  String getDescription() {
+    return 'Value must be a valid Hex color code';
+  }
+
+  @override
+  String getType() {
+    return Types.string.name;
+  }
+
+  @override
+  bool isArray() {
+    return false;
+  }
+
+  @override
+  bool isValid(dynamic value) {
+    if (value is String &&
+        RegExp('/^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\$/').hasMatch(value)) {
+      return true;
+    }
+    return false;
+  }
+}
